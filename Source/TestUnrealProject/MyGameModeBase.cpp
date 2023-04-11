4ce7cb5c50d6ee03969f9a7ffbf389784d86bc89
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2023-04-12 00:00:01 +0900

    230411

--- Source/TestUnrealProject/MyGameModeBase.cpp
+++ Source/TestUnrealProject/MyGameModeBase.cpp
@@ -6,5 +6,11 @@
 
 AMyGameModeBase::AMyGameModeBase()
 {
-	DefaultPawnClass = AMyCharacter::StaticClass();	//생성자에서 클래스를 스태틱 클래스로 상속받아온다. 언리얼 함수
+	//DefaultPawnClass = AMyCharacter::StaticClass();	//생성자에서 클래스를 스태틱 클래스로 상속받아온다. 언리얼 함수
+	static ConstructorHelpers::FClassFinder<ACharacter> BP_Char(TEXT("Blueprint'/Game/BluePrints/BP_MyCharacter.BP_MyCharacter_C'"));
+
+	if (BP_Char.Succeeded())
+	{
+		DefaultPawnClass = BP_Char.Class;
+	}
 }
