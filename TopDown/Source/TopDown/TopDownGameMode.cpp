9907144b8b542f4b2e548ccdde635c3fe1b612c8
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2023-07-31 23:58:47 +0900

    Add new Sample
    Top Down

---
+++ TopDown/Source/TopDown/TopDownGameMode.cpp
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TopDownGameMode.h"
+#include "TopDownPlayerController.h"
+#include "TopDownCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATopDownGameMode::ATopDownGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = ATopDownPlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != nullptr)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
